9c3598024047232f34a673764728825c74264229
Jonathan Ross <jonathan@jibo.com>
2016-08-23 16:30:14 -0400

    Open dev tools automatically

--- main.js
+++ main.js
@@ -4,40 +4,6 @@ var Menu = require('electron').Menu;
 var mainWindow = null;
 var path = require('path');
 
-var template = [
-    {
-        label: 'Electron',
-        submenu: [
-            {
-                label: 'Quit',
-                accelerator: 'Command+Q',
-                click: function () {
-                    app.quit();
-                }
-            },
-        ]
-    },
-    {
-        label: 'View',
-        submenu: [
-            {
-                label: 'Reload',
-                accelerator: 'Command+R',
-                click: function () {
-                    BrowserWindow.getFocusedWindow().reloadIgnoringCache();
-                }
-            },
-            {
-                label: 'Toggle DevTools',
-                accelerator: 'Alt+Command+I',
-                click: function () {
-                    BrowserWindow.getFocusedWindow().toggleDevTools();
-                }
-            }
-        ]
-    }
-];
-
 app.on('window-all-closed', function () {
     if (process.platform != 'darwin')
         app.quit();
@@ -45,12 +11,10 @@ app.on('window-all-closed', function () {
 
 
 app.on('ready', function () {
-    //set the context menu
-    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-
     mainWindow = new BrowserWindow({width: 1280, height: 720, resizable: true, title: 'Jibo Programming Challenge'});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
+    mainWindow.webContents.openDevTools({mode:'right'});
 });
